49e0f0b2e79fe3b3da6a15895a03c5f0f4bdf235
Merituum <bartolopoland@gmail.com>
2025-08-30 14:42:06 +0200

    added start focus, stop focus, schedule and in the middle of widget showing function

--- src/main.py
+++ src/main.py
@@ -87,6 +87,86 @@ class TaskForceController:
             self.tray_icon.show()
    # @todo set name and icon 
 
+    def start_focus_session(self):
+        if self.is_session_active:
+            QMessageBox.information(self.main_gui, "Info", "Session is arleady in progress!")
+            return
+        
+        duration_seconds = self._get_session_duration_from_gui()
+        break_interval = self._get_break_interval_from_gui()
+
+        if duration_seconds <= 0:
+            QMessageBox.warning(self.main_gui, "Error", "Incorrect duration of session!")
+            return
+        
+        try:
+            self.network_blocker.start()
+            self.session_start_time = datetime.now()
+            self.session_duration() == duration_seconds
+            self.break_interval = break_interval
+            self.remaining_time = duration_seconds
+
+            self._show_countdown_widget(duration_seconds)
+
+            self.session_start_time(duration_seconds * 1000)
+            self.break_timer.start(break_interval * 1000)
+
+            self.main_gui.hide()
+
+            QMessageBox.information(self.main_gui,"Success", f"Focus session started - duration: {duration_seconds}s,\n break every: {break_interval}s. ")
+        except Exception as e:
+            QMessageBox.critical(self.main_gui, "Error", f"Could not start session :( \n {str(e)}")
+    
+    def stop_focus_session(self):
+        if not self.is_session_active:
+            QMessageBox.information(self.main_gui,"Info", "No session is active")
+            return
+        try:
+            self.network_blocker.stop()
+            self.process_blocker.stop_blocking()
+
+            self.session_timer.stop()
+            self.break_timer.stop()
+
+            if self.countdown_widget:
+                self.countdown_widget.close()
+                self.countdown_widget = None
+            
+            self.is_session_active = False
+            self.session_start_time = None
+            self.main_gui.show()
+            self.main_gui.raise_()
+            self.main_gui.activateWindow()
+            QMessageBox.information(self.main_gui, "Finished!", "Focus session has been finished! \\ Well done!")
+            print("TaskForce: Session stopped by an user.")
+        except Exception as ee:
+            QMessageBox.critical(self.main_gui, "Error", f"An error occured while stopping a session :( \n {str(ee)}")
+    
+    def schedule_session(self):
+        start_time = self._get_scheduled_start_time_from_gui()
+
+        if not start_time or start_time <= datetime.now():
+            QMessageBox.warning(self.main_gui, "Error", "Incorrect time of beginning")
+            return
+        time_to_start = (start_time - datetime.now()).total_seconds()
+        QTimer.singleShot(int(time_to_start * 1000), self.start_focus_session)
+        QMessageBox.information(self.main_gui, "Scheduled!", f"Session will be started at {start_time.strifttime('%H:%M')}")
+        print(f"TaskForce: Session has been scheduled at {start_time}")
+    def _show_countdown_widget(self, seconds):
+        self.countdown_widget = SlidingCountdown(seconds_left=seconds)
+        self.countdown_widget.show()
+
+
+
+
+            
+
+
+
+       
+
+
+
        
     
        
